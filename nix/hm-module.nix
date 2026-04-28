3fd97e80ba80742074d9ff2e65f100dc0ae721bc
vMohammad24 <62218284+vMohammad24@users.noreply.github.com>
2026-04-28 16:12:38 +0300

    feat(nix): Add home-manager module (programs.framr)

---
+++ nix/hm-module.nix
@@ -0,0 +1,49 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  cfg = config.programs.framr;
+  tomlFormat = pkgs.formats.toml {};
+in {
+  options.programs.framr = {
+    enable = lib.mkEnableOption "framr";
+    package = lib.mkOption {
+      type = lib.types.package;
+      description = "The framr package to use.";
+    };
+    settings = lib.mkOption {
+      type = tomlFormat.type;
+      default = {};
+      example = lib.literalExpression ''
+        {
+          default_action = "Save";
+          default_capture = "Area";
+          uploaders = [
+            {
+              name = "My Uploader";
+              request_method = "POST";
+              request_url = "https://example.com/upload";
+              body_type = "FormData";
+              file_form_name = "file";
+              output_url = "{json:url}";
+            }
+          ];
+        }
+      '';
+      description = ''
+        Configuration written to {file}`$XDG_CONFIG_HOME/framr/default-config.toml`.
+        See the project's documentation for more information on the available options.
+      '';
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.packages = [cfg.package];
+
+    xdg.configFile."framr/default-config.toml" = lib.mkIf (cfg.settings != {}) {
+      source = tomlFormat.generate "framr-config" cfg.settings;
+    };
+  };
+}
