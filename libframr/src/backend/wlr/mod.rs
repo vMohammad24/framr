2bcd51e097354f0caada3767fca02332c3a52024
vMohammad24 <62218284+vMohammad24@users.noreply.github.com>
2026-05-07 02:55:28 +0300

    refactor(lib): move encoding logic from wlr/mod.rs to lib.rs

--- libframr/src/backend/wlr/mod.rs
+++ libframr/src/backend/wlr/mod.rs
@@ -19,9 +19,6 @@ use crate::error::FramrError;
 use crate::output::{FrameFormat, LogicalRegion, OutputInfo, PixelFormat, Position, Size};
 use crate::transform::apply_transform;
 
-use gstreamer::prelude::*;
-use gstreamer_app::AppSrc;
-
 mod dispatch;
 use dispatch::*;
 
@@ -35,26 +32,6 @@ fn pixel_format_to_wl_shm(fmt: PixelFormat) -> wayland_client::protocol::wl_shm:
 		PixelFormat::Abgr2101010 => WlFormat::Abgr2101010,
 		PixelFormat::Xbgr2101010 => WlFormat::Xbgr2101010,
 	}
-}
-
-fn wait_for_gstreamer_eos(pipeline: &gstreamer::Pipeline) -> Result<()> {
-	let bus = pipeline.bus().unwrap();
-	for msg in bus.iter_timed(gstreamer::ClockTime::NONE) {
-		use gstreamer::MessageView;
-		match msg.view() {
-			MessageView::Eos(..) => break,
-			MessageView::Error(err) => {
-				return Err(anyhow::anyhow!(
-					"GStreamer error: {} ({})",
-					err.error(),
-					err.debug().unwrap_or_else(|| "no debug info".into())
-				));
-			}
-			_ => (),
-		}
-	}
-	pipeline.set_state(gstreamer::State::Null)?;
-	Ok(())
 }
 
 fn allocate_shm_buffer<T>(
@@ -613,111 +590,12 @@ impl CaptureBackend for WlrBackend {
 
 		let transform = output.transform;
 		let pipeline_thread = std::thread::spawn(move || -> Result<()> {
-			let flip_method = match transform {
-				crate::output::Transform::Normal => "none",
-				crate::output::Transform::_90 => "clockwise",
-				crate::output::Transform::_180 => "rotate-180",
-				crate::output::Transform::_270 => "counterclockwise",
-				crate::output::Transform::Flipped => "horizontal-flip",
-				crate::output::Transform::Flipped90 => "upper-left-diagonal",
-				crate::output::Transform::Flipped180 => "vertical-flip",
-				crate::output::Transform::Flipped270 => "upper-right-diagonal",
-			};
-
-			let pipeline_str = format!(
-				"appsrc name=src format=time is-live=true ! videoconvert ! videoflip method={} ! x264enc tune=zerolatency speed-preset=ultrafast bitrate=4000 key-int-max=60 ! mp4mux ! filesink location={}",
-				flip_method,
-				output_path.to_string_lossy()
-			);
-
-			let pipeline = gstreamer::parse::launch(&pipeline_str)
-				.map_err(|e| anyhow::anyhow!("Failed to launch GStreamer pipeline. Ensure gst-plugins-good and gst-plugins-ugly (for x264enc) are installed. Error: {}", e))?
-				.dynamic_cast::<gstreamer::Pipeline>()
-				.map_err(|_| anyhow::anyhow!("Failed to cast to Pipeline"))?;
-
-			let appsrc = pipeline
-				.by_name("src")
-				.ok_or_else(|| anyhow::anyhow!("Failed to find appsrc"))?
-				.dynamic_cast::<AppSrc>()
-				.map_err(|_| anyhow::anyhow!("Failed to cast to AppSrc"))?;
-
-			appsrc.set_max_bytes(1024 * 1024 * 50);
-			appsrc.set_property("emit-signals", false);
-			appsrc.set_property("is-live", true);
-
-			pipeline.set_state(gstreamer::State::Ready)?;
-
-			let (mmap, buffer_idx, pts, format) = frame_receiver.recv()?;
-
-			let gst_format = match format.format {
-				PixelFormat::Argb8888 => gstreamer_video::VideoFormat::Bgra,
-				PixelFormat::Xrgb8888 => gstreamer_video::VideoFormat::Bgrx,
-				PixelFormat::Abgr8888 => gstreamer_video::VideoFormat::Rgba,
-				PixelFormat::Xbgr8888 => gstreamer_video::VideoFormat::Rgbx,
-				_ => {
-					return Err(anyhow::anyhow!(
-						"Unsupported pixel format for recording: {:?}",
-						format.format
-					));
-				}
-			};
-
-			let caps = gstreamer_video::VideoCapsBuilder::new()
-				.format(gst_format)
-				.width(format.width)
-				.height(format.height)
-				.framerate(gstreamer::Fraction::new(0, 1))
-				.build();
-
-			appsrc.set_caps(Some(&caps));
-
-			pipeline.set_state(gstreamer::State::Playing)?;
-
-			let mut buffer = gstreamer::Buffer::with_size(mmap.len())
-				.map_err(|_| anyhow::anyhow!("Failed to create buffer"))?;
-
-			{
-				let buffer_mut = buffer.get_mut().unwrap();
-				buffer_mut.set_pts(gstreamer::ClockTime::from_nseconds(pts));
-				buffer_mut
-					.copy_from_slice(0, &mmap)
-					.map_err(|_| anyhow::anyhow!("Failed to copy to buffer"))?;
-			}
-
-			appsrc.push_buffer(buffer)?;
-
-			let _ = return_sender.send(buffer_idx);
-
-			let mut previous_pts = pts;
-
-			while let Ok((mmap, buffer_idx, pts, _format)) = frame_receiver.recv() {
-				let mut buffer = gstreamer::Buffer::with_size(mmap.len())
-					.map_err(|_| anyhow::anyhow!("Failed to create buffer"))?;
-
-				{
-					let buffer_mut = buffer.get_mut().unwrap();
-					buffer_mut.set_pts(gstreamer::ClockTime::from_nseconds(pts));
-
-					if pts > previous_pts {
-						let duration = gstreamer::ClockTime::from_nseconds(pts - previous_pts);
-						buffer_mut.set_duration(Some(duration));
-					}
-
-					buffer_mut
-						.copy_from_slice(0, &mmap)
-						.map_err(|_| anyhow::anyhow!("Failed to copy to buffer"))?;
-				}
-
-				appsrc.push_buffer(buffer)?;
-
-				let _ = return_sender.send(buffer_idx);
-
-				previous_pts = pts;
-			}
-
-			appsrc.end_of_stream()?;
-			wait_for_gstreamer_eos(&pipeline)?;
-			Ok(())
+			crate::encoding::run_single_encoding_pipeline(
+				transform,
+				output_path,
+				frame_receiver,
+				return_sender,
+			)
 		});
 
 		Ok(RecordingHandle {
@@ -799,9 +677,6 @@ impl CaptureBackend for WlrBackend {
 			.map(|(_, o)| o.scale.max(1))
 			.max()
 			.unwrap_or(1) as i32;
-
-		let composite_width = (region.size.width as i32 * max_scale + 1) / 2 * 2;
-		let composite_height = (region.size.height as i32 * max_scale + 1) / 2 * 2;
 
 		let num_outputs = intersecting.len();
 		let (stop_sender, stop_receiver) = crossbeam_channel::bounded(1);
@@ -974,182 +849,18 @@ impl CaptureBackend for WlrBackend {
 			});
 		}
 
+		let encoder_outputs = intersecting.iter().map(|(_, o)| o.clone()).collect();
 		let pipeline_thread = std::thread::spawn(move || -> Result<()> {
-			let pipeline = gstreamer::Pipeline::new();
-
-			let mut appsrcs = Vec::with_capacity(num_outputs);
-			let compositor = gstreamer::ElementFactory::make("compositor")
-				.build()
-				.map_err(|_| anyhow::anyhow!("Failed to create compositor"))?;
-
-			compositor.set_property("background", 0u32);
-			compositor.set_property("width", composite_width);
-			compositor.set_property("height", composite_height);
-
-			let videoconvert = gstreamer::ElementFactory::make("videoconvert")
-				.build()
-				.map_err(|_| anyhow::anyhow!("Failed to create videoconvert"))?;
-
-			let encoder = gstreamer::ElementFactory::make("x264enc")
-				.build()
-				.map_err(|_| anyhow::anyhow!("Failed to create x264enc"))?;
-
-			encoder.set_property("tune", 0x00000004i32);
-			encoder.set_property("speed-preset", 7i32);
-			encoder.set_property("bitrate", 4000u32);
-			encoder.set_property("key-int-max", 60i32);
-
-			let muxer = gstreamer::ElementFactory::make("mp4mux")
-				.build()
-				.map_err(|_| anyhow::anyhow!("Failed to create mp4mux"))?;
-
-			let sink = gstreamer::ElementFactory::make("filesink")
-				.build()
-				.map_err(|_| anyhow::anyhow!("Failed to create filesink"))?;
-
-			sink.set_property("location", output_path.to_string_lossy().as_ref());
-
-			pipeline.add_many(&[&compositor, &videoconvert, &encoder, &muxer, &sink])?;
-			gstreamer::Element::link_many(&[&compositor, &videoconvert, &encoder, &muxer, &sink])?;
-
-			for (i, (_, output)) in intersecting.iter().enumerate() {
-				let appsrc = gstreamer::ElementFactory::make("appsrc")
-					.name(&format!("src_{}", i))
-					.build()
-					.map_err(|_| anyhow::anyhow!("Failed to create appsrc"))?;
-
-				appsrc.set_property("format", gstreamer::Format::Time);
-				appsrc.set_property("is-live", true);
-
-				pipeline.add(&appsrc)?;
-
-				let sink_pad = compositor
-					.request_pad_simple(&format!("sink_{}", i))
-					.ok_or_else(|| anyhow::anyhow!("Failed to get sink pad"))?;
-
-				let src_pad = appsrc
-					.static_pad("src")
-					.ok_or_else(|| anyhow::anyhow!("Failed to get src pad"))?;
-
-				src_pad.link(&sink_pad)?;
-
-				let rel_x = output.logical_position.x - region.position.x;
-				let rel_y = output.logical_position.y - region.position.y;
-
-				sink_pad.set_property("xpos", rel_x * max_scale);
-				sink_pad.set_property("ypos", rel_y * max_scale);
-				sink_pad.set_property("width", output.logical_size.width as i32 * max_scale);
-				sink_pad.set_property("height", output.logical_size.height as i32 * max_scale);
-				sink_pad.set_property("zorder", 0i32);
-
-				appsrcs.push((appsrc, output.clone()));
-			}
-
-			pipeline.set_state(gstreamer::State::Ready)?;
-
-			let mut frame_formats = Vec::with_capacity(num_outputs);
-			for (i, format_receiver) in format_receivers.iter().enumerate() {
-				let frame_format = format_receiver
-					.recv()
-					.map_err(|_| anyhow::anyhow!("Failed to receive initial format"))?;
-				frame_formats.push(frame_format.clone());
-
-				let gst_format = match frame_format.format {
-					PixelFormat::Argb8888 => gstreamer_video::VideoFormat::Bgra,
-					PixelFormat::Xrgb8888 => gstreamer_video::VideoFormat::Bgrx,
-					PixelFormat::Abgr8888 => gstreamer_video::VideoFormat::Rgba,
-					PixelFormat::Xbgr8888 => gstreamer_video::VideoFormat::Rgbx,
-					_ => {
-						return Err(anyhow::anyhow!(
-							"Unsupported pixel format for recording: {:?}",
-							frame_format.format
-						));
-					}
-				};
-
-				let caps = gstreamer_video::VideoCapsBuilder::new()
-					.format(gst_format)
-					.width(frame_format.width)
-					.height(frame_format.height)
-					.framerate(gstreamer::Fraction::new(0, 1))
-					.build();
-
-				appsrcs[i].0.set_property("caps", &caps);
-			}
-
-			pipeline.set_state(gstreamer::State::Playing)?;
-
-			let mut start_pts = None;
-			let mut previous_pts_vec: Vec<Option<u64>> = vec![None; num_outputs];
-
-			let mut select = crossbeam_channel::Select::new();
-			for i in 0..num_outputs {
-				select.recv(&frame_receivers[i]);
-			}
-			select.recv(&stop_receiver);
-
-			loop {
-				let oper = select.select();
-				let index = oper.index();
-
-				if index == num_outputs {
-					let _ = oper.recv(&stop_receiver);
-					break;
-				} else {
-					let i = index;
-					if let Ok((mmap, buffer_idx, pts, _frame_format)) =
-						oper.recv(&frame_receivers[i])
-					{
-						let (appsrc, _) = &appsrcs[i];
-
-						if start_pts.is_none() {
-							start_pts = Some(pts);
-						}
-						let relative_pts = pts.saturating_sub(start_pts.unwrap());
-
-						let mut buffer = gstreamer::Buffer::with_size(mmap.len())
-							.map_err(|_| anyhow::anyhow!("Failed to create buffer"))?;
-
-						{
-							let buffer_mut = buffer.get_mut().unwrap();
-							buffer_mut.set_pts(gstreamer::ClockTime::from_nseconds(relative_pts));
-
-							if let Some(prev) = previous_pts_vec[i] {
-								if relative_pts > prev {
-									let duration =
-										gstreamer::ClockTime::from_nseconds(relative_pts - prev);
-									buffer_mut.set_duration(Some(duration));
-								}
-							}
-
-							buffer_mut
-								.copy_from_slice(0, &*mmap)
-								.map_err(|e| anyhow::anyhow!("copy_from_slice failed: {e}"))?;
-						}
-
-						let appsrc_ref = appsrc.clone();
-						appsrc_ref
-							.downcast::<AppSrc>()
-							.map_err(|_| anyhow::anyhow!("Failed to cast to AppSrc"))?
-							.push_buffer(buffer)?;
-
-						let _ = return_senders[i].send(buffer_idx);
-
-						previous_pts_vec[i] = Some(relative_pts);
-					}
-				}
-			}
-
-			for (appsrc, _) in &appsrcs {
-				let appsrc_ref = appsrc.clone();
-				appsrc_ref
-					.downcast::<AppSrc>()
-					.map_err(|_| anyhow::anyhow!("Failed to cast to AppSrc"))?
-					.end_of_stream()?;
-			}
-
-			wait_for_gstreamer_eos(&pipeline)?;
-			Ok(())
+			crate::encoding::run_composite_encoding_pipeline(
+				output_path,
+				region,
+				max_scale,
+				encoder_outputs,
+				frame_receivers,
+				format_receivers,
+				return_senders,
+				stop_receiver,
+			)
 		});
 
 		Ok(RecordingHandle {
--- libframr/src/lib.rs
+++ libframr/src/lib.rs
@@ -2,10 +2,10 @@ pub mod backend;
 mod buffer;
 mod connection;
 mod convert;
+mod encoding;
 mod error;
 mod output;
 mod transform;
-
 pub use connection::FramrConnection;
 pub use error::FramrError;
 pub use output::{FrameFormat, LogicalRegion, OutputInfo, PixelFormat, Position, Size, Transform};
